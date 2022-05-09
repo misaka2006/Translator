8d6f2df4ade12f23dbd90557e08a485f3f3ec104
Lijiajun <minecraft_li2006@qq.com>
2022-05-09 22:43:11 +0800

    完成

---
+++ Translator/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.FromComboBox = new System.Windows.Forms.ComboBox();
+            this.ToComboBox = new System.Windows.Forms.ComboBox();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(3, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(132, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "翻译";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(194, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(131, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // FromComboBox
+            // 
+            this.FromComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.FromComboBox.FormattingEnabled = true;
+            this.FromComboBox.Location = new System.Drawing.Point(44, 41);
+            this.FromComboBox.Name = "FromComboBox";
+            this.FromComboBox.Size = new System.Drawing.Size(91, 20);
+            this.FromComboBox.TabIndex = 4;
+            this.FromComboBox.SelectedIndexChanged += new System.EventHandler(this.FromComboBox_SelectedIndexChanged);
+            // 
+            // ToComboBox
+            // 
+            this.ToComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ToComboBox.FormattingEnabled = true;
+            this.ToComboBox.Location = new System.Drawing.Point(251, 41);
+            this.ToComboBox.Name = "ToComboBox";
+            this.ToComboBox.Size = new System.Drawing.Size(75, 20);
+            this.ToComboBox.TabIndex = 5;
+            this.ToComboBox.SelectedIndexChanged += new System.EventHandler(this.ToComboBox_SelectedIndexChanged);
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Location = new System.Drawing.Point(3, 67);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(75, 52);
+            this.button_Clear.TabIndex = 6;
+            this.button_Clear.Text = "清空";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "源语言";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "目标语言";
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(250, 67);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(75, 52);
+            this.buttonCopy.TabIndex = 9;
+            this.buttonCopy.Text = "复制";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 131);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.ToComboBox);
+            this.Controls.Add(this.FromComboBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "翻译器";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox FromComboBox;
+        private System.Windows.Forms.ComboBox ToComboBox;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonCopy;
+    }
+}
+
---
+++ Translator/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Translator
+{
+    public partial class Form1 : Form
+    {
+        private TransHelper transHelper = new TransHelper();
+        Hashtable hashtable = new Hashtable();
+        string f, t;
+        string[] lang = new string[] {"自动识别", "汉语", "英语", "日语", "韩语", "文言文", "粤语" };
+        public Form1()
+        {
+            InitializeComponent();
+            hashtable.Add("自动识别", "auto");
+            hashtable.Add("汉语", "zh");
+            hashtable.Add("英语", "en");
+            hashtable.Add("日语", "jp");
+            hashtable.Add("韩语", "kor");
+            hashtable.Add("文言文", "wyw");
+            hashtable.Add("粤语", "yue");
+            
+            
+            foreach (string v in lang)
+            {
+                FromComboBox.Items.Add(v);
+                ToComboBox.Items.Add(v);
+            }
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //{"from":"zh","to":"en","trans_result":[{"src":"\u4f60\u597d","dst":"Hello"}]}
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //button1.Dispose();
+            Console.WriteLine(f);
+            Console.WriteLine(t);
+            string res="";
+            try
+            {
+                f = (string)hashtable[FromComboBox.SelectedItem.ToString()];
+                t = (string)hashtable[ToComboBox.SelectedItem.ToString()];
+                string str = transHelper.Trans(textBox1.Text,f,t);
+                JObject jObject = (JObject)JsonConvert.DeserializeObject(str);
+                JArray jArray = (JArray)JsonConvert.DeserializeObject(jObject["trans_result"].ToString());
+                res = jArray[0]["dst"].ToString();
+            }
+            catch(NullReferenceException)
+            {
+                MessageBox.Show("请选择语言！", "发生异常！", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch(WebException)
+            {
+                MessageBox.Show("网络请求超时！", "发生异常！", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                textBox2.Text = res;
+            }
+            
+        }
+
+        private void FromComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //f = hashtable[lang[FromComboBox.SelectedIndex]]
+        }
+
+        private void ToComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            t = ToComboBox.SelectedItem.ToString();
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox2.Text);
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text = "";
+        }
+    }
+}
